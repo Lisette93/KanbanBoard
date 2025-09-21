6b4be1569244ab3ebfedaaca1e890df40999b49e
Liset <lisettestorm@hotmail.com>
2025-09-21 16:12:40 +0200

    changed to HashRouting for gh pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,15 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import App from "./App";
 import BoardPage from "./pages/BoardPage";
 import ColumnPage from "./pages/ColumnPage";
 
-const basename = import.meta.env.BASE_URL;
-
-export const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        { path: "/", element: <BoardPage /> },
-        { path: "/column/:columnId", element: <ColumnPage /> },
-      ],
-    },
-  ],
-  { basename }
-);
+export const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <BoardPage /> },
+      { path: "/column/:columnId", element: <ColumnPage /> },
+    ],
+  },
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/KanbanBoard",
+  base: "/KanbanBoard/",
   plugins: [react(), tailwind()],
 });
